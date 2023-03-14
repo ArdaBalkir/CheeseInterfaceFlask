9192d79c857d0d88d2f812b9092cf2e9ad952911
Arda <abalkir2014@gmail.com>
2023-03-14 23:36:22 +0000

    Brightness and Saturation

--- custom_methods.py
+++ custom_methods.py
@@ -4,18 +4,16 @@ import numpy as np
 # Returns hole_number, hole_to_surface_ratio
 def holiness(image_array):
 
-    #img = cv2.imread(image_path)
     gray = cv2.cvtColor(image_array, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
 
     # Apply Otsu's thresholding to create a binary image
     _, thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-    # Perform morphological opening to remove small objects
+    # Remove small objects
     kernel = np.ones((3, 3), np.uint8)
     opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
 
-    # Find contours in the binary image
     contours, _ = cv2.findContours(opening, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     # Create a mask image for holes
@@ -43,17 +41,15 @@ def holiness(image_array):
     params.filterByCircularity = False
     params.filterByArea = False
 
-    # Perform blob detection to identify the holes
     detector = cv2.SimpleBlobDetector_create(params)
     keypoints = detector.detect(thresh)
 
-    # Print the number of holes detected
     print("Number of holes detected:", len(keypoints))
 
     # Get the amount of white pixels on the mask image
     cheese_material_surface = np.count_nonzero(mask)
 
-    # Combine the threshold image and mask image
+    # Combine the threshold image and mask image to get rid of the background
     combined = cv2.bitwise_or(thresh, holes_mask)
     # This is the final image we can also return for demonstration
     
@@ -61,15 +57,34 @@ def holiness(image_array):
     holes_surface = np.count_nonzero(combined <= 100)
     hole_to_surface_ratio = holes_surface / cheese_material_surface
 
-    # Print the hole-to-surface ratio
     print("Hole-to-surface ratio:", "{:.3f}".format(hole_to_surface_ratio))
 
     # Perform blob detection to identify the holes in the combined image
-    # This step is a failsafe, 
+    # This step is a failsafe
     keypoints1 = detector.detect(combined) 
 
-    # Print the number of holes detected in the combined image
+    # Print the number of holes detected in the combined image for debugging
     print("Number of holes detected in the combined image:", len(keypoints1))
     
     return len(keypoints), "{:.3f}".format(hole_to_surface_ratio)
 
+# Returns sat and br disregarding the background
+def get_saturation_and_brightness(image_array):
+
+    img_hsv = cv2.cvtColor(image_array, cv2.COLOR_BGR2HSV)
+    sat = img_hsv[:,:,1]
+    bright = img_hsv[:,:,2]
+
+    # Applying masks to Saturation and Brightness
+    sat_mask = sat >= 10
+    bright_mask = bright >= 30
+
+    # Our limits have been chosen after consulting a colour picker
+    # Masks are applied
+    sat_filtered = sat[sat_mask]
+    bright_filtered = bright[bright_mask]
+
+    # The average/mean is calculated
+    mean_sat = np.mean(sat_filtered)
+    mean_bright = np.mean(bright_filtered)
+    return round(mean_sat, 2), round(mean_bright, 2)
--- report_funcs.py
+++ report_funcs.py
@@ -9,8 +9,8 @@ def report_cheddar(image_array):
 def report_emmantal(image_array):
 
     hole_count, hole_to_surface_ratio = holiness(image_array)
-    
-    return f'The cheese type is Emmantal, with {hole_count} holes and {hole_to_surface_ratio} hole to surface ratio!'
+    sat, bright = get_saturation_and_brightness(image_array)
+    return f'The cheese type is Emmantal, with {hole_count} holes and {hole_to_surface_ratio} hole to surface ratio!\nThe cheese has a brightness of {bright} out of 255 and saturation of {sat} out of 255'
 
 def report_leicester(image):
     return 'leicester report'
