4c4af1869a4868ed81e08fcd4d08e98f684d037c
Arda <abalkir2014@gmail.com>
2023-04-09 19:15:15 +0100

    fixed prediction

--- CEUtilities.py
+++ CEUtilities.py
@@ -39,38 +39,3 @@ def ce_prediction(image, image_np):
     #probability_score = model.predict_proba(image_array)[0][predicted_class]
 
     return current_report
-
-def api_prediction(image, image_np):
-    # Preprocessing the image
-    image = image.resize((32, 32))  # Resize the image to the same size as the model's input shape
-    image_array = np.array(image)
-    image_array = image_array.reshape(1, 32, 32, 3)  # Add a batch dimension to the image
-
-    # Convert the image to base64
-    buffered = BytesIO()
-    image.save(buffered, format="JPEG")
-    img_str = base64.b64encode(buffered.getvalue()).decode('utf-8')
-
-    # Set the API endpoint
-    url = "https://your-api-url.com/predict"
-
-    # Create the payload for the POST request
-    payload = {
-        "image": img_str
-    }
-
-    # Send the POST request
-    response = requests.post(url, json=payload)
-
-    # Check if the request was successful
-    if response.status_code == 200:
-        # Get the predicted class from the API response
-        predicted_class = json.loads(response.text)["predicted_class"]
-
-        # current_report is set to the returned string output
-        current_report = report_gen[predicted_class](image_np)
-        # Input above is the unadultered image to properly asses the cheese features
-
-        return current_report
-    else:
-        return "Error: Could not get prediction from the API"
